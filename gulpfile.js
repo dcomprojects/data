7a60306318186ab288ad9620688da8330a1e1680
Dave O'Mahony <david.c.omahony@gmail.com>
2020-03-16 21:08:16 -0230

    initial workbox

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,14 @@ const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
 const cleancss = require('gulp-clean-css');
+const del = require('del');
+
+const workboxBuild = require('workbox-build');
+
+// Clean "build" directory
+function clean() {
+  return del(['build/*'], {dot: true});
+};
 
 function copy() {
     return gulp.src([
@@ -22,6 +30,26 @@ function serve() {
         port: 3000
     });
 }
+
+function buildSw() {
+    return workboxBuild.injectManifest({
+      swSrc: 'build/scripts/sw.min.js',
+      swDest: 'build/scripts/sw.imin.js',
+      globDirectory: 'build',
+      globPatterns: [
+        '**\/*.css',
+        'index.html',
+        'js\/animation.js',
+        'images\/home\/*.jpg',
+        'images\/icon\/*.svg',
+      ]
+    }).then(resources => {
+      console.log(`Injected ${resources.count} resources for precaching, ` +
+          `totaling ${resources.size} bytes.`);
+    }).catch(err => {
+      console.log('Uh oh 😬', err);
+    });
+  }
 
 function processJs() {
     return gulp.src('app/scripts/*.js')
@@ -59,12 +87,14 @@ function watch() {
     gulp.parallel(watchCss, watchJs);
 }
 
-gulp.task('watch', watch) 
+gulp.task('watch', watch);
 
 gulp.task('buildAndServe', gulp.series(
+    clean,
 	copy, 
 	processJs, 
-	processCss, 
-	gulp.parallel(watchCss, watchJs, serve
-	)
-));
+    processCss, 
+    buildSw,
+    gulp.parallel(watchCss, watchJs, serve)
+    )
+);
