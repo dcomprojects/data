fe2e38ed54db8a2894a5255bbfded965b2b50154
Dave O'Mahony <david.c.omahony@gmail.com>
2020-03-18 06:43:43 -0230

    template tweaks

--- gulpfile.js
+++ gulpfile.js
@@ -4,23 +4,26 @@ const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
 const cleancss = require('gulp-clean-css');
+const filter = require('gulp-filter');
 const del = require('del');
 
 const workboxBuild = require('workbox-build');
 
 // Clean "build" directory
 function clean() {
-  return del(['build/*'], {dot: true});
+  return del(['build/*'], {
+    dot: true
+  });
 };
 
 function copy() {
-    return gulp.src([
+  return gulp.src([
       'app/*.html',
       'app/**/*.jpg',
       'app/**/*.svg',
       'app/**/*.png',
       'app/**/*.scss',
-      'app/webfonts/**',
+      'app/**/webfonts/*',
     ])
     .pipe(gulp.dest('build'));
 }
@@ -28,38 +31,38 @@ function copy() {
 gulp.task('copy', copy);
 
 function serve() {
-    return browserSync.init({
-        server: 'build',
-        open: false,
-        port: 3000
-    });
+  return browserSync.init({
+    server: 'build',
+    open: false,
+    port: 3000
+  });
 }
 
 function buildSw() {
-    return workboxBuild.injectManifest({
-      swSrc: 'app/sw.js',
-      swDest: 'build/sw.min.js',
-      globDirectory: 'build',
-      globPatterns: [
-          '**',
-        ],
-      globIgnores: [
-        'sw.js'
-      ] 
-    }).then(resources => {
-      console.log(`Injected ${resources.count} resources for precaching, ` +
-          `totaling ${resources.size} bytes.`);
-    }).catch(err => {
-      console.log('Uh oh 😬', err);
-    });
-  }
+  return workboxBuild.injectManifest({
+    swSrc: 'app/sw.js',
+    swDest: 'build/sw.min.js',
+    globDirectory: 'build',
+    globPatterns: [
+      '**',
+    ],
+    globIgnores: [
+      'sw.js'
+    ]
+  }).then(resources => {
+    console.log(`Injected ${resources.count} resources for precaching, ` +
+      `totaling ${resources.size} bytes.`);
+  }).catch(err => {
+    console.log('Uh oh 😬', err);
+  });
+}
 
 function processJs() {
-    return gulp.src(['app/assets/js/*.js'])
-    .pipe(babel({
-        presets: ['env']
-    }))
-    .pipe(uglify())
+  return gulp.src(['app/assets/js/*.js'])
+    //.pipe(babel({
+    //  presets: ['env']
+    //}))
+    //.pipe(uglify())
     //.pipe(rename({
     //    suffix: '.min'
     //}))
@@ -69,11 +72,16 @@ function processJs() {
 gulp.task('processJs', processJs);
 
 function watchJs() {
-    gulp.watch('app/assets/js/*.js', processJs);
+  gulp.watch('app/assets/js/*.js', processJs);
 }
 
 function processCss() {
-    return gulp.src('app/assets/css/*.css')
+  return gulp.src('app/assets/css/*.css')
+    .pipe(filter([
+      'app/assets/css/main.css',
+      'app/assets/css/noscript.css',
+      'app/assets/css/fontawesome-all.min.css'
+    ]))
     .pipe(cleancss())
     //.pipe(rename({
     //    suffix: '.min'
@@ -82,22 +90,22 @@ function processCss() {
 }
 
 gulp.task('processCss', processCss)
+
 function watchCss() {
-    gulp.watch('app/assets/css/*.css', processCss);
+  gulp.watch('app/assets/css/*.css', processCss);
 }
 
 function watch() {
-    gulp.parallel(watchCss, watchJs);
+  gulp.parallel(watchCss, watchJs);
 }
 
 gulp.task('watch', watch);
 
 gulp.task('buildAndServe', gulp.series(
-    clean,
-	copy, 
-	processJs, 
-    processCss, 
-    buildSw,
-    gulp.parallel(watchCss, watchJs, serve)
-    )
-);
+  clean,
+  copy,
+  processJs,
+  processCss,
+  buildSw,
+  gulp.parallel(watchCss, watchJs, serve)
+));
